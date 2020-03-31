855fa696c7425fe1e7e3da678b26cf16ffb44e9f
shanzm <2351144690@qq.com>
2020-03-31 22:49:26 +0800

    2020年3月31日 22:48:08- Create：计算器1.0
    非面向对象，实现最原始的四则计算器功能

---
+++ 01简单工厂模式/计算器1.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using static System.Console;
+
+
+namespace 计算器1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            double numA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter another number");
+            double numB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter operation");
+            string oper = Console.ReadLine();
+
+            double result=0;
+
+            switch (oper)
+            {
+                case "+":
+                    result = numA + numB;
+                    break;
+                case "-":
+                    result = numA - numB;
+                    break;
+                case "*":
+                    result = numA * numB;
+                    break;
+                case "/":
+                 result= numB != 0 ? numA/numB : 0;
+                    break;
+                //default:result = 0;
+                //    break;
+            }
+
+
+            Console.WriteLine(result.ToString ());
+            Console.ReadKey();
+        }
+    }
+}
