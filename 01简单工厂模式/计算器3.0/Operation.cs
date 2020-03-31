ddb4d0492f2bf0f8ce78024fd0c07824f1727d11
shanzm <2351144690@qq.com>
2020-03-31 22:52:44 +0800

    2020年3月31日 22:51:51- Create：计算器V3.0
    开始使用面向对象，封装一个方法类

---
+++ 01简单工厂模式/计算器3.0/Operation.cs
@@ -0,0 +1,49 @@
+﻿#region Version Info 
+// ===============================================================================
+// Project Name        :    计算器3._0  
+// ===============================================================================
+// Class Name          :    Operation
+// Namespace           :    计算器3._0 
+// Class Version       :    v1.0.0.0
+// Class Description   : 
+// CLR                 :    4.0.30319.42000  
+// Author              :    shanzm
+// Create Time         :    2019/1/22 1:24:07
+// Update Time         :    2019/1/22 1:24:07
+// ===============================================================================
+// Copyright © SHANZM-PC  2019 . All rights reserved.
+// ===============================================================================
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 计算器3._0
+{
+    public class Operation
+    {
+        public static double GetResult(double numA, double numB, string oper)
+        {
+            double result = 0;
+            switch (oper)
+            {
+                case "+":
+                    result = numA + numB;
+                    break;
+                case "-":
+                    result = numA - numB;
+                    break;
+                case "*":
+                    result = numA * numB;
+                    break;
+                case "/":
+                    result = numB != 0 ? numA / numB : 0;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ 01简单工厂模式/计算器3.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 计算器3._0
+{
+    class Program
+    {
+        //让业务逻辑和界面逻辑分离
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter a number");
+                double numA = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("enter another number");
+                double numB = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("enter operation");
+                string oper = Console.ReadLine();
+
+                Console.WriteLine(Operation.GetResult(numA, numB, oper).ToString());
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("error:" + e.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
