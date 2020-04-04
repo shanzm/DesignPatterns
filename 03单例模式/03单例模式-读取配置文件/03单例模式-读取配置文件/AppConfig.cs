b4767e0f3c5c5ac60f071fb45f64c54e002ee9b1
shanzm <2351144690@qq.com>
2020-04-05 01:12:26 +0800

    2020年4月5日 01:11:29- Create：03单例模式-读取配置文件
    实现读取配置文件的类只能实例化一个

---
+++ 03单例模式/03单例模式-读取配置文件/03单例模式-读取配置文件/AppConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03单例模式_读取配置文件
+{
+    public class AppConfig
+    {
+        //定义字段，用于存储唯一实例
+        private static AppConfig appConfig = null;
+
+        //对应配置文件设置相应的属性，注意是只读的
+        public string Server { private set; get; }
+        public string DataBase { private set; get; }
+        public string UserId { private set; get; }
+        public string PassWord { private set; get; }
+
+        //构造函数私有化
+        private AppConfig()
+        {
+
+        }
+
+        //获取唯一实例
+        public static AppConfig GetInstance()
+        {
+            if (appConfig == null)
+            {
+                appConfig = new AppConfig()
+                {
+                    Server = ConfigurationManager.AppSettings["server"],
+                    DataBase = ConfigurationManager.AppSettings["databaser"],
+                    UserId = ConfigurationManager.AppSettings["uid"],
+                    PassWord = ConfigurationManager.AppSettings["pwd"]
+                };
+            }
+            return appConfig;
+        }
+
+    }
+}
---
+++ 03单例模式/03单例模式-读取配置文件/03单例模式-读取配置文件/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _03单例模式_读取配置文件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppConfig appConfig1 = AppConfig.GetInstance();
+            string connectionString = $"server={appConfig1.Server},database={appConfig1.DataBase},uid ={appConfig1.UserId},pwd ={appConfig1.PassWord}";
+            Console.WriteLine(connectionString);
+      
+            AppConfig appConfig2 = AppConfig.GetInstance();
+
+            Console.WriteLine(object.ReferenceEquals(appConfig1, appConfig2));//print:true。即系统中的appConfig对象是同一个
+
+            Console.ReadKey();
+        }
+    }
+}
