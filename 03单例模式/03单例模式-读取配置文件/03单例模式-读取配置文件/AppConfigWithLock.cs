711f1912299c469e6d521d2365707c1d406adb48
shanzm <2351144690@qq.com>
2020-04-06 00:46:22 +0800

    2020年4月6日 00:45:13- Update：对创建单例对象的操作加锁
    为了编写线程安全的代码，防止多线程同时创建需要单例的对象，对创建函数GetInstance（）加锁

---
+++ 03单例模式/03单例模式-读取配置文件/03单例模式-读取配置文件/AppConfigWithLock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03单例模式_读取配置文件
+{
+    class AppConfigWithLock
+    {
+        //定义字段，用于存储唯一实例
+        private static AppConfigWithLock appConfig = null;
+
+        //对应配置文件设置相应的属性，注意是只读的
+        public string Server { private set; get; }
+        public string DataBase { private set; get; }
+        public string UserId { private set; get; }
+        public string PassWord { private set; get; }
+
+        //构造函数私有化
+        private AppConfigWithLock()
+        {
+            Server = ConfigurationManager.AppSettings["server"];
+            DataBase = ConfigurationManager.AppSettings["databaser"];
+            UserId = ConfigurationManager.AppSettings["uid"];
+            PassWord = ConfigurationManager.AppSettings["pwd"];
+        }
+
+        private static readonly object asyncRoot = new object();
+
+        //获取唯一实例
+        public static AppConfigWithLock GetInstance()
+        {
+            if (appConfig == null)
+            {
+                lock (asyncRoot)
+                {
+                    if (appConfig == null)
+                    {
+                        appConfig = new AppConfigWithLock();
+                    }
+                }
+
+            }
+            return appConfig;
+        }
+
+    }
+}
