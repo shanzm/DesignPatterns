3141693e6736f1a324e157cd0457f34df7284b3b
shanzm <2351144690@qq.com>
2020-04-06 00:47:25 +0800

    2020年4月6日 00:46:30- Update：饿汉式单例模式
    类加载的时候创建单例对象

---
+++ 03单例模式/03单例模式-读取配置文件/03单例模式-读取配置文件/AppConfigStatic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace _03单例模式_读取配置文件
+{
+    public sealed class AppConfig2
+    {
+        //定义字段，用于存储唯一实例
+        private static readonly AppConfig2 appConfig = new AppConfig2();
+
+        //对应配置文件设置相应的属性，注意是只读的
+        public string Server { private set; get; }
+        public string DataBase { private set; get; }
+        public string UserId { private set; get; }
+        public string PassWord { private set; get; }
+
+        //构造函数私有化
+        private AppConfig2()
+        {
+            Server = ConfigurationManager.AppSettings["server"];
+            DataBase = ConfigurationManager.AppSettings["databaser"];
+            UserId = ConfigurationManager.AppSettings["uid"];
+            PassWord = ConfigurationManager.AppSettings["pwd"];
+        }
+
+        //获取唯一实例
+        public static AppConfig2 GetInstance()
+        {
+            return appConfig;
+        }
+
+    }
+}
