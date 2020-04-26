c8a9dffe2c8ee901736a5f761a25ddf18cca65e8
shanzm <2351144690@qq.com>
2020-04-26 22:26:00 +0800

    2020年4月26日 22:25:45- Update：修改细节

--- 04抽象工厂模式/04抽象工厂模式-多数据库连接-使用简单工厂改进/DatabaseFactory.cs
+++ 04抽象工厂模式/04抽象工厂模式-多数据库连接-使用简单工厂改进/DatabaseFactory.cs
@@ -9,7 +9,7 @@ namespace _04抽象工厂模式_多数据库连接_使用简单工厂改进
     //其实就是简单工厂中的有针对不同的表，分别实现该表的工厂方法
     //若是一个表，则就是一个工厂方法，就和之前的普通的简单工厂模式一模一样了。
 
-    class DatabaseFactory
+    public class DatabaseFactory
     {
         private static readonly string db = "MSSQL";//若是需要更换数据库则将字符串改为"Oracle"
         public static IUserService CreateUserService()
